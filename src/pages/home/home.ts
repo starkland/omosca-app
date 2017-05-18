731c9c37267eb22d39d294f3b479a0d00fb52e33
Thulio Philipe <thulioph@gmail.com>
2017-05-18 18:54:09 -0300

    Adds initial slider on home page.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,30 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  slides: Array<{
+    title: string,
+    description: string,
+    image: string
+  }>
 
+  constructor(public navCtrl: NavController) {
+    this.slides = [
+      {
+        title: 'Welcome to OMOSCA',
+        description: 'The <b>Ionic Component Documentation</b> showcases a number of useful components that are included out of the box with Ionic.',
+        image: 'http://dummyimage.com/420x420/4d494d/686a82.gif&text=imagem+teste'
+      },
+      {
+        title: 'Hey MOSCA!',
+        description: 'The <b>Ionic Component Documentation</b> showcases a number of useful components that are included out of the box with Ionic.',
+        image: 'http://dummyimage.com/420x420/4d494d/686a82.gif&text=imagem+teste'
+      },
+      {
+        title: 'Bye bro',
+        description: 'The <b>Ionic Component Documentation</b> showcases a number of useful components that are included out of the box with Ionic.',
+        image: 'http://dummyimage.com/420x420/4d494d/686a82.gif&text=imagem+teste'
+      }
+    ];
   }
 
 }
