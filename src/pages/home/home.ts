ba05235df79ecde81a0f1b38faee4b7bc1d06389
Thulio Philipe <thulioph@gmail.com>
2017-05-18 19:13:18 -0300

    Some adjusts on home page.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -17,18 +17,18 @@ export class HomePage {
     this.slides = [
       {
         title: 'Welcome to OMOSCA',
-        description: 'The <b>Ionic Component Documentation</b> showcases a number of useful components that are included out of the box with Ionic.',
-        image: 'http://dummyimage.com/420x420/4d494d/686a82.gif&text=imagem+teste'
+        description: 'The showcases a number of useful components that are included out of the box..',
+        image: 'http://dummyimage.com/420x520/4d494d/686a82.gif&text=imagem+teste'
       },
       {
         title: 'Hey MOSCA!',
-        description: 'The <b>Ionic Component Documentation</b> showcases a number of useful components that are included out of the box with Ionic.',
-        image: 'http://dummyimage.com/420x420/4d494d/686a82.gif&text=imagem+teste'
+        description: 'The <b>Ionic Component Documentation</b> showcases a number of useful components.',
+        image: 'http://dummyimage.com/420x520/4d494d/686a82.gif&text=imagem+teste'
       },
       {
         title: 'Bye bro',
-        description: 'The <b>Ionic Component Documentation</b> showcases a number of useful components that are included out of the box with Ionic.',
-        image: 'http://dummyimage.com/420x420/4d494d/686a82.gif&text=imagem+teste'
+        description: 'The <b>Ionic Component Documentation</b> showcases a number of useful components.',
+        image: 'http://dummyimage.com/420x520/4d494d/686a82.gif&text=imagem+teste'
       }
     ];
   }
