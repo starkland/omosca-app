3fd291fa0a11f921e03b157614c892ff75595479
Thulio Philipe <thulioph@gmail.com>
2017-05-18 19:57:24 -0300

    Adds some blank spaces.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -20,11 +20,13 @@ export class HomePage {
         description: 'The showcases a number of useful components that are included out of the box..',
         image: 'http://dummyimage.com/420x520/4d494d/686a82.gif&text=imagem+teste'
       },
+
       {
         title: 'Hey MOSCA!',
         description: 'The <b>Ionic Component Documentation</b> showcases a number of useful components.',
         image: 'http://dummyimage.com/420x520/4d494d/686a82.gif&text=imagem+teste'
       },
+
       {
         title: 'Bye bro',
         description: 'The <b>Ionic Component Documentation</b> showcases a number of useful components.',
