281002ade0ef4b6aeb0745f6f5cfd96f12da5068
Thulio Philipe <thulioph@gmail.com>
2017-05-18 20:55:31 -0300

    Adds the initial markup to login page.

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the LoginPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 @IonicPage()
 @Component({
   selector: 'page-login',
@@ -21,4 +15,7 @@ export class LoginPage {
     console.log('ionViewDidLoad LoginPage');
   }
 
+  login() {
+    console.warn('Login com o Facebook..');
+  }
 }
