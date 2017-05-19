f1c0a25e473f0b06e02213a300f1de1329e675b0
thulioph <thulioph@gmail.com>
2017-05-19 01:11:44 -0300

    Adds the initial content to favorites page.

--- src/pages/favorites/favorites.ts
+++ src/pages/favorites/favorites.ts
@@ -10,7 +10,30 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class FavoritesPage {
 
+  cards: Array<{
+    image: string,
+    title: string,
+    description: string,
+    id: number,
+  }>
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+    this.cards = [
+      {
+        id: 1,
+        title: 'Mr. Taco',
+        image: 'http://dummyimage.com/600x390/4d494d/686a82.gif&text=imagem+da+balada',
+        description: 'The most popular industrial group ever, and largely responsible for bringing the music to a mass audience.',
+      },
+
+      {
+        title: `Beto's Bar`,
+        id: 2,
+        image: 'http://dummyimage.com/600x390/4d494d/686a82.gif&text=imagem+da+balada',
+        description: 'The most popular industrial group ever, and largely responsible for bringing the music to a mass audience.',
+      },
+    ];
   }
 
   ionViewDidLoad() {
