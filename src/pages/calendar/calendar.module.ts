b4dfb83b0e73d001fcd1c13f9226bf252bccd6fd
Thulio Philipe <thulioph@gmail.com>
2017-05-22 14:16:12 -0300

    Display the initial calendar component.

--- src/pages/calendar/calendar.module.ts
+++ src/pages/calendar/calendar.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { CalendarPage } from './calendar';
 
+import { NgCalendarModule  } from 'ionic2-calendar';
+
 @NgModule({
   declarations: [
     CalendarPage,
   ],
   imports: [
+    NgCalendarModule,
     IonicPageModule.forChild(CalendarPage),
   ],
   exports: [
--- src/pages/calendar/calendar.ts
+++ src/pages/calendar/calendar.ts
@@ -10,11 +10,37 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CalendarPage {
 
+  calendar: Object;
+  viewTitle: String;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+    this.calendar = {
+      mode: 'month',
+      currentDate: '',
+    };
+
+    this.viewTitle = '';
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CalendarPage');
   }
 
+  onCurrentDateChanged = (ev: Date) => {
+    console.warn('Currently viewed date:', ev);
+  };
+
+  onEventSelected = (event) => {
+    console.warn('onEventSelected:', event.title);
+  };
+
+  onViewTitleChanged = (title: string) => {
+    console.warn(title);
+    this.viewTitle = title;
+  };
+
+  onTimeSelected = (ev: { selectedTime: Date, events: any[] }) => {
+    console.warn('Selected time: ' + ev.selectedTime + ', hasEvents: ' + (ev.events !== undefined && ev.events.length !== 0));
+  };
 }
