8507da66ffc2a5c54481925cfd203559a02658e0
Thulio Philipe <thulioph@gmail.com>
2017-05-26 17:06:23 -0300

    Get the user data from Facebook and save on local storage.

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -12,10 +12,14 @@ import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook';
 })
 export class LoginPage {
 
+  scopes: any = [];
+
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     private fb: Facebook) {
+
+    this.scopes = ['public_profile', 'email'];
   }
 
   ionViewDidLoad() {
@@ -23,24 +27,45 @@ export class LoginPage {
   }
 
   login() {
-    const scopes = ['public_profile', 'email'];
-
     this.fb
-      .login(scopes)
+      .login(this.scopes)
       .then(this._handleLogin.bind(this))
       .catch(this._handleError.bind(this));
   }
 
-  _handleLogin(res) {
-    console.warn(res.authResponse);
+  getUserInfo() {
+    this.fb
+      .api('/me?fields=email,id,name', this.scopes)
+      .then(this._handleApi.bind(this))
+      .catch(this._handleError.bind(this));
+  }
 
-    // setTimeout(() => {
-    //   this.navCtrl.push('dashboard');
-    // }, 300);
+  _handleLogin(res) {
+    if (res.status !== 'connected') {
+      console.warn('O status do login foi:', res.status);
+    } else {
+      const fbInfo = {
+        accessToken: res.authResponse.accessToken,
+        userID: res.authResponse.userID
+      };
+
+      // Set some facebook data on session storage
+      localStorage.setItem('fb_info', JSON.stringify(fbInfo));
+
+      this.getUserInfo();
+    }
+  }
+
+  _handleApi(obj) {
+    // Set the user data on session storage
+    localStorage.setItem('user_info', JSON.stringify(obj));
+
+    setTimeout(() => {
+      this.navCtrl.push('dashboard');
+    }, 300);
   }
 
   _handleError(err) {
-    alert('ERROR!');
-    console.info(err);
+    console.info('Algum erro aconteceu: ', err);
   }
 }
