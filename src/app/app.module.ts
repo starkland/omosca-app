aef125d6f65f83d7c7f970f73ef896af07ec5a22
Thulio Philipe <thulioph@gmail.com>
2017-05-26 15:56:38 -0300

    Removes the loginpage from app module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
-import { LoginPage } from '../pages/login/login';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -18,7 +17,6 @@ import 'intl/locale-data/jsonp/en';
     MyApp,
     HomePage,
     ListPage,
-    LoginPage,
   ],
   imports: [
     BrowserModule,
@@ -29,7 +27,6 @@ import 'intl/locale-data/jsonp/en';
     MyApp,
     HomePage,
     ListPage,
-    LoginPage,
   ],
   providers: [
     StatusBar,
