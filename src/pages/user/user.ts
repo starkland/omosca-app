015ab0dcae571ce9475d59ca547e4297ebf25be8
thulioph <thulioph@gmail.com>
2017-06-04 17:05:02 -0300

    Display the user profile data on Profile page.

--- src/pages/user/user.ts
+++ src/pages/user/user.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {
+  IonicPage,
+  NavController,
+  NavParams
+} from 'ionic-angular';
+
+import { LocalStorage } from '../../services/LocalStorage';
 
 @IonicPage({
   name: 'user',
@@ -9,16 +15,37 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'user.html',
 })
 export class UserPage {
+  userData: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public storage: LocalStorage) {
+
+    this.userData = {};
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad UserPage');
+    this.displayUserInfo();
+  }
+
+  displayUserInfo() {
+    const userInfo = this.storage.get('user_info');
+
+    if (!userInfo) {
+      // chama pra API.
+    } else {
+      this.userData = userInfo;
+      this.userData.photo = userInfo.picture.data.url;
+    }
   }
 
   logout() {
-    this.navCtrl.push('login');
-    console.warn('O usuário saiu da aplicação.');
+    this.storage.remove('user_info');
+
+    setTimeout(() => {
+      this.navCtrl.push('login');
+    }, 300);
   }
 }
