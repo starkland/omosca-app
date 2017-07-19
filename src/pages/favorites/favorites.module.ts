d1a25f432700bf13368c847a939f8cc42b284da2
thulioph <thulioph@gmail.com>
2017-07-19 02:10:19 -0300

    Working to display the favorite data using the modal.

--- src/pages/favorites/favorites.module.ts
+++ src/pages/favorites/favorites.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { FavoritesPage } from './favorites';
+import { FavCard } from './favorite-modal';
 
 @NgModule({
   declarations: [
     FavoritesPage,
+    FavCard,
   ],
   imports: [
     IonicPageModule.forChild(FavoritesPage),
   ],
   exports: [
     FavoritesPage
-  ]
+  ],
+  entryComponents: [
+    FavCard,
+  ],
 })
 export class FavoritesPageModule {}
