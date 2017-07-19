c4abf756913ca712fcdfda2e8ae58286ca66a68f
Thulio Philipe <thulioph@gmail.com>
2017-07-19 15:03:44 -0300

    Display the favorite data using the modal.

--- src/pages/favorites/favorite-modal.ts
+++ src/pages/favorites/favorite-modal.ts
@@ -1,21 +1,28 @@
 import { Component } from '@angular/core';
-import {
-   NavParams,
-   ViewController
-} from 'ionic-angular';
+import { NavParams, ViewController } from 'ionic-angular';
 
 @Component({
   templateUrl: 'modal.html',
 })
 export class FavCard {
+  placeData: any;
+
   constructor(
-    params: NavParams,
-    public viewCtrl: ViewController) {
-    console.warn(params.get('favId'));
+    public params: NavParams,
+    public viewCtrl: ViewController
+    ) {
+
+    if (params.get('favId')) {
+      this.placeData = params.get('favId');
+    }
   }
 
   dismiss() {
-   let data = { 'foo': 'bar' };
+   let data = {};
    this.viewCtrl.dismiss(data);
+  }
+
+  openEvent(obj) {
+    console.warn('Evento clicado: ', obj);
   }
 }
