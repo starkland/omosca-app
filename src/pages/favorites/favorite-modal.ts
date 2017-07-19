91c0dcfe645de10e7b3bbc1949c7ef7abb3ac2ae
Thulio Philipe <thulioph@gmail.com>
2017-07-19 14:43:11 -0300

    Adjusts the favorite modal component.

--- src/pages/favorites/favorite-modal.ts
+++ src/pages/favorites/favorite-modal.ts
@@ -1,32 +1,21 @@
 import { Component } from '@angular/core';
-import { NavParams } from 'ionic-angular';
+import {
+   NavParams,
+   ViewController
+} from 'ionic-angular';
 
 @Component({
-  selector: 'favcard',
-  template: `
-  <ion-header>
-    <ion-toolbar>
-      <ion-title>
-        Description
-      </ion-title>
-
-      <ion-buttons start>
-        <button ion-button (click)="dismiss()">
-          <span ion-text color="primary" showWhen="ios">Cancel</span>
-          <ion-icon name="md-close" showWhen="android, windows"></ion-icon>
-        </button>
-      </ion-buttons>
-    </ion-toolbar>
-  </ion-header>
-
-  <ion-content>
-    <ion-list></ion-list>
-  </ion-content>
-  `,
-  // templateUrl: 'modal.html',
+  templateUrl: 'modal.html',
 })
 export class FavCard {
- constructor(params: NavParams) {
-   console.log('UserId', params.get('favId'));
- }
+  constructor(
+    params: NavParams,
+    public viewCtrl: ViewController) {
+    console.warn(params.get('favId'));
+  }
+
+  dismiss() {
+   let data = { 'foo': 'bar' };
+   this.viewCtrl.dismiss(data);
+  }
 }
